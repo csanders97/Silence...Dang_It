7907cc774fd128fd589ea48bff23353f6d47c77d
Justine Barry <justinianfruit@gmail.com>
2018-08-09 23:01:42 -0600

    Admin page created with links to edit page, needs styling to be finished.

--- admin.php
+++ admin.php
@@ -4,11 +4,39 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <script src="theme1.css"></script>
+        <link rel="stylesheet" type="text/css" href="theme1.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Hanalei+Fill|Josefin+Slab|Open+Sans|Poiret+One|Ultra" rel="stylesheet">
         <title>CMS - SILENCE</title>
     </head>
     <body>
         <?php include 'header.php' ?>
         <h1>Welcome, Admin</h1>
+        <h3>Current Page Hierarchy</h3>
+        <?php
+            header("Access-Control-Allow-Origin: *");
+
+            include 'dbconfig.php';
+
+            $getContent = "select * from pages where parent='None'";
+            $result = $mysqli->query( $getContent );
+            $num_results = $result->num_rows;
+            if( $num_results > 0) {
+                while( $row = $result->fetch_assoc() ) {
+                    extract($row);
+                    echo "<div class='main'>{$html}<a class='editBtn' href='edit.php'>Edit &#10000;</a></div>";
+                    $getSubContent = "select * from pages where parent = '$category'";
+                    $subResult = $mysqli->query( $getSubContent );
+                    $num_sub = $subResult->num_rows;
+                    if( $num_sub > 0) {
+                        while( $row = $subResult->fetch_assoc() ) {
+                            extract($row);
+                            echo "<div class='subcat'><span>&#9866;</span>{$html}<a class='editBtn' href='edit.php'>Edit &#10000;</a></div>";
+                        }
+                    }
+                }
+                $result->free();
+                $mysqli->close();
+            }
+        ?>
     </body>
 </html>
