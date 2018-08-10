63278b6b9843e145bbe5f75d1586320c291b8e90
swargo <swargo@student.neumont.edu>
2018-08-09 20:51:52 -0600

    Basic themes done and switching between them works, just need to finish up styling so they each are more unique rather than just different colors and fonts

--- base.php
+++ base.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="theme1.css"></script>
+    <?php
+        if(isset($_GET["theme"])) {
+            $themeQ = urldecode($_GET["theme"]);
+        } else {
+            $themeQ = "theme1";
+        }
+    ?>
+    <link rel="stylesheet" type="text/css" href=<?php echo $themeQ ?> + ".css"/>
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Hanalei+Fill|Josefin+Slab|Open+Sans|Poiret+One|Ultra" rel="stylesheet">
     <title>CMS - SILENCE</title>
 </head>
 <body>
--- header.php
+++ header.php
@@ -7,12 +7,16 @@
     $result = $mysqli->query( $getHeader );
     $num_results = $result->num_rows;
     if( $num_results > 0) {
-        echo "<header>";
+        echo "<header><ul>";
             while( $row = $result->fetch_assoc() ) {
                 extract($row);
-                echo "<div><a href='base.php?page={$category}'>{$category}</a></div>";
+                echo "<a href='base.php?page={$category}'><li>{$category}</li></a>";
             }
-        echo "</header>";
+        echo "</ul><ul class='right'>";
+        echo "<a href='base.php?page={$category}&theme=theme1'><li>Theme 1</li></a>";
+        echo "<a href='base.php?page={$category}&theme=theme2'><li>Theme 2</li></a>";
+        echo "<a href='base.php?page={$category}&theme=theme3'><li>Theme 3</li></a>";
+        echo "</ul></header>";
         $result->free();
         $mysqli->close();
     }
